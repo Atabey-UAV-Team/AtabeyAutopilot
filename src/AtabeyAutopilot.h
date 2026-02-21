6ad08c5583be4f77c117231b8fd7f436e020a267
furkankuyumcu54 <kuyumcufurkan05@gmail.com>
2026-02-22 02:37:35 +0300

    Namespace bugs hotfixed.

---
+++ src/AtabeyAutopilot.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "core/Autopilot.h"
+#include "core/Scheduler.h"
+#include "core/FlightModeManager.h"
+#include "core/FailsafeManager.h"
+#include "core/HealthMonitor.h"
+#include "core/ParameterStore.h"
