e6e3e4388b370a0217a8b9107c5016b4f297f942
furkankuyumcu54 <kuyumcufurkan05@gmail.com>
2026-03-11 22:33:47 +0300

    Gps Driver changed to UBX protocol instead of NMEA parsing

--- src/drivers/actuators/servo.h
+++ src/drivers/actuators/servo.h
@@ -3,17 +3,18 @@
 #include "Arduino.h"
 #include "IActuator.h"
 
-
-
 namespace atabey {
     namespace drivers {
 
         template<uint8_t ELEVON_SOL_PIN, uint8_t ELEVON_SAG_PIN>
-        class ServoPWM : public atabey::drivers::IActuator {ü
+        class ServoPWM : public atabey::drivers::IActuator {
             private:
-                static constexpr uint8_t SERVO_MIN = -20; // Derece cinsinden minimum servo açısı
-                static constexpr uint8_t SERVO_MAX = 20;  // Derece cinsinden maksimum servo açısı
+                static constexpr int8_t SERVO_MIN = -20; // Derece cinsinden minimum servo açısı
+                static constexpr int8_t SERVO_MAX = 20;  // Derece cinsinden maksimum servo açısı
 
+                int8_t solAngle = 0; // Sol elevon açısı (derece cinsinden)
+                int8_t sagAngle = 0; // Sağ elevon açısı (derece cinsinden)
+                
             public:
                 ServoPWM() {}
 
@@ -24,6 +25,8 @@ namespace atabey {
                 }
 
                 void setPosition(float solAngle, float sagAngle) {
+
+                    // Gelen açıları sınırla ve 0-255 aralığına dönüştür
                     uint8_t solAci = (constrain(solAngle, SERVO_MIN, SERVO_MAX) - SERVO_MIN) * 255 / (SERVO_MAX - SERVO_MIN);
                     uint8_t sagAci = (constrain(sagAngle, SERVO_MIN, SERVO_MAX) - SERVO_MIN) * 255 / (SERVO_MAX - SERVO_MIN);
 
--- src/drivers/sensors/gps.cpp
+++ src/drivers/sensors/gps.cpp
@@ -1,124 +1,160 @@
-#include "GpsDriver.h"
+#include "GpsSensor.h"
+
+#define GPS_SERIAL Serial1
+#define GPS_BAUDRATE 38400
+
+#define UBX_CLASS_NAV 0x01
+#define UBX_ID_NAV_PVT 0x07
 
 namespace atabey {
-  namespace drivers{
-   
-    bool GpsDriver::init(){
-      bool buffer; //state control
-      Serial1.begin(9600); //start communication with gps
-      if(Serial1.avaliable() != 0){
-        buffer = true; //if gps is avaliable, set state 1
-        digitalWrite(31, 1); //if gps is working, blink LED connected to digital 31
-      }else() buffer = false; // if gps isn't working, set state 0
-      return buffer; // return stat
-    }
+    namespace drivers {
 
-    void GpsDriver::update(){
-      char data, arr[75], oldData = '\0';
-      int8_t remainder = 0, index = -1, int count = 0;
-      char data;
-      uint32_t north, northDegree, northMinute, east, eastDegree, eastMinute;
-      float northAngle, altitude = 0.0, decimalMultiplier = 0.1;
-      bool isDecimal = false, isNegative = false;
-      
-      if(Serial1.available()){ 
-        data = Serial1.read();
-        
-        if(data == 'A' && oldData == 'G'){
-          remainder = 74; 
-        }else{
-          if(remainder > 0){
-            arr[74 - remainder] = data; 
-            remainder--; 
-          }
-          else if(remainder == 0){
-            for(int i=11; i<=21; i++) arr[i] -= 48; 
-            for(int i=24; i<=34; i++) arr[i] -= 48; 
-            
-            northDegree = 10 * arr[11] + arr[12];
-            northMinute = 1000000 * arr[13] + 100000 * arr[14] + 10000 * arr[16] + 1000 * arr[17] + 100 * arr[18] + 10 * arr[19] + arr[20];
-            north = 6000000 * northDegree + northMinute;
-          
-            northAngle = (float)northDegree + (((float)northMinute)/6000000.0) * 0.01745329251994329576923690768489;
-            eastDegree = 100 * arr[24] + 10 * arr[25] + arr[26];
-            eastMinute = 1000000 * arr[27] + 100000 * arr[28] + 10000 * arr[30] + 1000 * arr[31] + 100 * arr[32] + 10 * arr[33] + arr[34];
-            east = (uint32_t)(((float)6000000 * eastDegree + eastMinute) * cos(northAngle) * 1.852);
-            
-            for(int i = 0; i < 74; i++) {
-                if(arr[i] == ',') {
-                    count++;
-                    if(count == 9) { 
-                        index = i + 1;
+        GpsSensor::GpsSensor() {
+            state = WAIT_SYNC1;
+        }
+
+        bool GpsSensor::init() {
+            GPS_SERIAL.begin(GPS_BAUDRATE);
+            state = WAIT_SYNC1;
+            return true;
+        }
+
+        void GpsSensor::update() {
+            while (GPS_SERIAL.available()) {
+                uint8_t c = GPS_SERIAL.read();
+                parseByte(c);
+            }
+
+        }
+
+        void GpsSensor::parseByte(uint8_t c) {
+            switch (state) {
+                case WAIT_SYNC1:
+                    if (c == 0xB5)
+                        state = WAIT_SYNC2;
+
+                    break;
+
+                case WAIT_SYNC2:
+                    if (c == 0x62)
+                        state = READ_CLASS;
+                    else
+                        state = WAIT_SYNC1;
+
+                    break;
+
+                case READ_CLASS:
+                    msgClass = c;
+                    ckA = ckB = 0;
+                    ckA += c;
+                    ckB += ckA;
+                    state = READ_ID;
+
+                    break;
+
+                case READ_ID:
+                    msgId = c;
+                    ckA += c;
+                    ckB += ckA;
+                    state = READ_LENGTH1;
+
+                    break;
+
+                case READ_LENGTH1:
+                    length = c;
+                    ckA += c;
+                    ckB += ckA;
+                    state = READ_LENGTH2;
+
+                    break;
+
+                case READ_LENGTH2:
+                    length |= (uint16_t)c << 8;
+
+                    ckA += c;
+                    ckB += ckA;
+
+                    counter = 0;
+                    if (length > sizeof(payload)) {
+
+                        state = WAIT_SYNC1;
                         break;
+
                     }
-                }
+                    state = READ_PAYLOAD;
+
+                    break;
+
+                case READ_PAYLOAD:
+                    payload[counter++] = c;
+                    ckA += c;
+                    ckB += ckA;
+
+                    if (counter >= length)
+                        state = READ_CKA;
+
+                    break;
+
+                case READ_CKA:
+                    if (c == ckA)
+                        state = READ_CKB;
+                    else
+                        state = WAIT_SYNC1;
+
+                    break;
+
+                case READ_CKB:
+                    if (c == ckB)
+                        processPacket();
+
+                    state = WAIT_SYNC1;
+
+                    break;
             }
-            
-            if (index != -1) {
-                if (arr[index] == '-') {
-                    isNegative = true;
-                    index++;
-                }
-                for(int i = index; i < 74; i++) {
-                    if(arr[i] == ',') break; 
-                    if(arr[i] == '.') {
-                        isDecimal = true;
-                    } 
-                    else if(arr[i] >= '0' && arr[i] <= '9') {
-                        if(!isDecimal) {
-                            altitude = (altitude * 10.0) + (arr[i] - 48);
-                        } else {
-                            altitude = altitude + ((arr[i] - 48) * decimalMultiplier);
-                            decimalMultiplier /= 10.0;
-                        }
-                    }
-                }
-                if (isNegative) altitude = -altitude;
+        }
+
+        void GpsSensor::processPacket() {
+            if (msgClass == UBX_CLASS_NAV && msgId == UBX_ID_NAV_PVT) {
+
+                if (length != sizeof(NavPVT))
+                    return;
+                    
+                NavPVT* nav = reinterpret_cast<NavPVT*>(payload);
+
+                gps.lat = nav->lat;
+                gps.lon = nav->lon;
+                gps.alt = nav->hMSL;
+
+                gps.velN = nav->velN;
+                gps.velE = nav->velE;
+                gps.velD = nav->velD;
+
+                gps.fixType = nav->fixType;
+
+                gps.lastUpdate = millis();
             }
-            lat = north;
-            lon = east;
-            alt = altitude;
-            remainder--;
-          }
         }
-        oldData = data;
-      }
-    }
 
-    bool GpsDriver::isHealthy(){
-      while(Serial1.available()){
-        unsigned long lastUpdate;
-        char identificationData = '\0';
-        bool state = false;
-        while(state == false){
-          identificationData = Serial1.read();
-          if (Serial1.available() != 0 && identificationData != '\0'){
-            lastUpdate = millis();
-            state = true;
+        bool GpsSensor::isHealthy() const {
+            // GPS verisi en fazla 1 saniye eski olabilir
+            return millis() - gps.lastUpdate < 2000;
         }
-        identificationData = '\0';
-        if(millis() - lastUpdate > 2000){
-          return false;
-        }else{
-          return true;
+
+        bool GpsSensor::hasFix() {
+            return gps.fixType >= 3;
         }
-      }
-    }
 
-    bool GpsDriver::hasFix(){
-      return(lat != 0 && lon != 0);
-    }
+        int32_t GpsSensor::getLat() {
+            return gps.lat;
+        }
 
-    double GpsDriver::getLat(){
-      return lat;
-    }
+        int32_t GpsSensor::getLon() {
+            return gps.lon;
+        }
 
-    double GpsDriver::getLon(){
-      return lon;
-    }
+        int32_t GpsSensor::getAlt() {
+            return gps.alt;
+        }
 
-    float GpsDriver::getAlt(){
-      return alt;
     }
-  }
 }
